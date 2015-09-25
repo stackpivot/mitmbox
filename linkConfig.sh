798930741f1047e688296c078db8d8fd5d717e6f
mitmboxJB <mitmbox@b4rg.de>
2015-09-25 12:30:05 +0000

    added hostapd.conf

--- linkConfig.sh
+++ linkConfig.sh
@@ -2,3 +2,6 @@
 /bin/ln -s /root/mitmbox/config/interfaces /etc/network/interfaces
 /bin/ln -s /root/mitmbox/config/swconfig /etc/network/if-pre-up.d/swconfig
 /bin/ln -s /root/mitmbox/config/renameInterfaces /etc/network/if-up.d/renameInterfaces
+/bin/ln -s /root/mitmbox/config/wifiAP/hostapd /usr/local/sbin/hostapd
+/bin/ln -s /root/mitmbox/config/wifiAP/hostapd_cli /usr/local/sbin/hostapd_cli
+/bin/ln -s /root/mitmbox/config/wifiAP/hostapd.conf /etc/hostapd.conf
