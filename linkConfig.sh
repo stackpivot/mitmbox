eb663d36e2f09714508d41c2d4e08380c62b7d13
mitmboxJB <mitmbox@b4rg.de>
2015-09-25 12:15:43 +0000

    interfaces running

--- linkConfig.sh
+++ linkConfig.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /bin/ln -s /root/mitmbox/config/interfaces /etc/network/interfaces
-/bin/ln -l /root/mitmbox/config/swconfig /etc/network/if-pre-up.d/swconfig
+/bin/ln -s /root/mitmbox/config/swconfig /etc/network/if-pre-up.d/swconfig
+/bin/ln -s /root/mitmbox/config/renameInterfaces /etc/network/if-up.d/renameInterfaces
