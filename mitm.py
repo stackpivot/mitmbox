8abbc5a46f1c224b4b2ff684a21d6f543bedde9b
jon <jon@b4rg.de>
2015-09-26 17:47:43 +0000

    new folder layout

--- mitm.py
+++ mitm.py
@@ -1,14 +1,32 @@
 from ConfigParser import *
+import argparse
 import os
 import sys
 
 
+dst_ip   = ''
+dst_mac  = ''
+dst_port = 0
+
+src_ip   = ''
+src_mac  = ''
+src_port = 0
 
 
 def parse_config():
 	config = ConfigParser()
 	config.readfp(open('mitm.conf'))
-	print config.get('Server','ip')
+
+	dst_ip   = config.get('Destination','ip')
+	dst_mac  = config.get('Destination','mac')
+	dst_port = config.get('Destination','port')
+
+	src_ip   = config.get('Source','ip')
+	src_mac  = config.get('Source','mac')
+	src_port = config.get('Source','port')
+
+
+	print config.get('Settings','monitoring port')
 
 def main():
 	print "hello"
