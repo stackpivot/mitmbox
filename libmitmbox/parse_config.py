b681f52795f244154886213f44e9e7ca203b230e
jon <jon@b4rg.de>
2015-10-12 14:23:44 +0000

    runs

--- libmitmbox/parse_config.py
+++ libmitmbox/parse_config.py
@@ -11,14 +11,15 @@ class Parse_MitmConfig():
     '''
     trigger_parsing is a seperate function, to reread the configuration file if needed. This feature might be implemented in the future
     '''
-    def trigger_parsing():
+
+    def trigger_parsing(self):
 
         # TODO Check if file exists
         self.config.readfp(open(self.configFile))
 
         self.dst_ip = self.config.get('Destination', 'ip')
         self.dst_mac = self.config.get('Destination', 'mac')
-        self.dst_port = config.get('Destination', 'port')
+        self.dst_port = self.config.get('Destination', 'port')
 
         self.src_ip = self.config.get('Source', 'ip')
         self.src_mac = self.config.get('Source', 'mac')
