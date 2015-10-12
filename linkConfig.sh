264243e4369ad0b388062927c46e158ced442de2
jon <jon@b4rg.de>
2015-10-12 10:38:43 +0000

    new folder name

--- linkConfig.sh
+++ linkConfig.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-/bin/ln -s /root/mitmbox/config/interfaces /etc/network/interfaces
-/bin/ln -s /root/mitmbox/config/swconfig /etc/network/if-pre-up.d/swconfig
-/bin/ln -s /root/mitmbox/config/renameInterfaces /etc/network/if-up.d/renameInterfaces
-/bin/ln -f -s /root/mitmbox/config/wifiAP/hostapd /usr/sbin/hostapd
-/bin/ln -f -s /root/mitmbox/config/wifiAP/hostapd_cli /usr/sbin/hostapd_cli
-/bin/ln -s /root/mitmbox/config/wifiAP/hostapd.conf /etc/hostapd.conf
-/bin/ln -f -s /root/mitmbox/config/wifiAP/default/hostapd /etc/default/hostapd
+/bin/ln -s /root/mitmbox/os_configuration/config/interfaces /etc/network/interfaces
+/bin/ln -s /root/mitmbox/os_configuration/config/swconfig /etc/network/if-pre-up.d/swconfig
+/bin/ln -s /root/mitmbox/os_configuration/config/renameInterfaces /etc/network/if-up.d/renameInterfaces
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/hostapd /usr/sbin/hostapd
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/hostapd_cli /usr/sbin/hostapd_cli
+/bin/ln -s /root/mitmbox/os_configuration/config/wifiAP/hostapd.conf /etc/hostapd.conf
+/bin/ln -f -s /root/mitmbox/os_configuration/config/wifiAP/default/hostapd /etc/default/hostapd
