eab111b3b3a5c301fbd67789ec3cf8be94583c75
jon <jon@b4rg.de>
2015-10-12 12:22:24 +0000

    a

--- libmitmbox/tapDevice.py
+++ libmitmbox/tapDevice.py
@@ -1,10 +1,5 @@
-import fcntl
 import os
 import struct
-
-TUNSETIFF = 0x400454ca  # attach to tun/tap device
-IFF_TAP = 0x0002    # utilize tap device, i.e. including ethernet layer
-IFF_NO_IP = 0x1000  # omit packet information that is added by kernel
 
 
 def init_tapDevices(bridge0_interface, bridge1_interface):
@@ -17,7 +12,3 @@ def init_tapDevices(bridge0_interface, bridge1_interface):
     os.system("ifconfig tap0 1.2.3.4")
     os.system("route add -net 192.168.0.0 netmask 255.255.0.0 tap0")
     # todo: ARP request for source IP must answered
-
-    tap_device = os.open('/dev/net/tun', os.O_RDWR)
-    flags = struct.pack('16sH', "tap0", IFF_TAP | IFF_NO_IP)
-    fcntl.ioctl(tap_device, TUNSETIFF, flags)
