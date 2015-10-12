14080881b852b9d3dd95bbff63d3fb3af8348ef5
jon <jon@b4rg.de>
2015-10-12 14:21:38 +0000

    added function to reread config file

--- libmitmbox/parse_config.py
+++ libmitmbox/parse_config.py
@@ -1,25 +1,29 @@
 from ConfigParser import *
-import pdb
 
 
 class Parse_MitmConfig():
 
     def __init__(self, configFile):
+        self.config = ConfigParser()
+        self.configFile = configFile
+        self.trigger_parsing()
 
-        # pdb.set_trace()
+    '''
+    trigger_parsing is a seperate function, to reread the configuration file if needed. This feature might be implemented in the future
+    '''
+    def trigger_parsing():
+
         # TODO Check if file exists
-        config = ConfigParser()
-        config.readfp(open(configFile))
+        self.config.readfp(open(self.configFile))
 
-        self.dst_ip = config.get('Destination', 'ip')
-        self.dst_mac = config.get('Destination', 'mac')
+        self.dst_ip = self.config.get('Destination', 'ip')
+        self.dst_mac = self.config.get('Destination', 'mac')
         self.dst_port = config.get('Destination', 'port')
 
-        self.src_ip = config.get('Source', 'ip')
-        self.src_mac = config.get('Source', 'mac')
-        self.src_port = config.get('Source', 'port')
+        self.src_ip = self.config.get('Source', 'ip')
+        self.src_mac = self.config.get('Source', 'mac')
+        self.src_port = self.config.get('Source', 'port')
 
-        self.bridge0_interface = config.get('Interfaces', 'bridge0')
-        self.bridge1_interface = config.get('Interfaces', 'bridge1')
-        self.mitm_interface = config.get('Interfaces', 'mitm')
-        # TODO: add interfaces to bridge
+        self.bridge0_interface = self.config.get('Interfaces', 'bridge0')
+        self.bridge1_interface = self.config.get('Interfaces', 'bridge1')
+        self.mitm_interface = self.config.get('Interfaces', 'mitm')
